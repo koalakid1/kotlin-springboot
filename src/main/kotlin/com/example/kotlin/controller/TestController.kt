3dbf14efef12204ed537647fa14e391243cf22fb
koalakid1 <koalakid154@gmail.com>
2020-10-25 13:32:51 +0900

    test mustache

--- src/main/kotlin/com/example/kotlin/controller/TestController.kt
+++ src/main/kotlin/com/example/kotlin/controller/TestController.kt
@@ -4,16 +4,16 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 class TestController {
 
     @RequestMapping("/")
-    fun index(): String {
+    fun index(model: Model): String {
         return "index"
     }
-
 }
